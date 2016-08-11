9cd307b29c64ff62399dfd27a021f28e97efae98
Kevin Rivas <krivas2433@gmail.com>
2016-08-11 11:17:07 -0400

    Routed from goingPage and Linked to gasPage

--- src/components/3-vehiclePage.js
+++ src/components/3-vehiclePage.js
@@ -1,8 +1,26 @@
-
+/* globals require, module */
 "use strict";
-
 var React = require('react');
+var {Link} = require('react-router');
+var RouteHandler = require('react-router').RouteHandler;
 
-var vehiclePage =
+
+var vehiclePage = React.createClass({
+  render: function() {
+    return (
+      <div className="jumbotron">
+        <h1>What are you driving?</h1>
+        <p>Select your vehicle.</p>
+        <hr/>
+        <p></p>
+        <p class="lead">
+          <Link to="gasPage" class="btn btn-primary btn-lg" role="button">Final step!!</Link>
+        </p>
+      </div>
+    );
+  }
+});
 
 module.exports = vehiclePage;
+
+//Popover button more more info.
