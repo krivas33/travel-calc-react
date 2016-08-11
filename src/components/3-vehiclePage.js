fc7523976671cefb63118b24cf8a928145075879
Kevin Rivas <krivas2433@gmail.com>
2016-08-11 13:23:59 -0400

    Added back button and added vehicleInput link/page

--- src/components/3-vehiclePage.js
+++ src/components/3-vehiclePage.js
@@ -13,6 +13,12 @@ var vehiclePage = React.createClass({
         <p>Select your vehicle.</p>
         <hr/>
         <p></p>
+          <p class="lead">
+            <Link to="vehicleInput" class="btn btn-primary btn-lg" role="button">Input your vehicle</Link>
+          </p>
+          <p class="lead">
+            <Link to="goingPage" class="btn btn-primary btn-lg" role="button">Back</Link>
+          </p>
         <p class="lead">
           <Link to="gasPage" class="btn btn-primary btn-lg" role="button">Final step!!</Link>
         </p>
