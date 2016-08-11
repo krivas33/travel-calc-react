b475f3defd75db5409f395174b31892b5cb517cc
Kevin Rivas <krivas2433@gmail.com>
2016-08-11 10:34:31 -0400

    Removed Handler to app.js, wrapped element in run/render

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 /* globals require, module */
-$ = jQuery = require('jquery');
-var React = require('react');
-var SplashPage = require('./components/0-splashPage');
-//var Router = require('react-router');
-//var routes = require('./routes');
+"use strict";
 
-React.render(<SplashPage />, document.getElementById('app'));
+var React = require('react');
+var Router = require('react-router');
+var routes = require('./routes');
+
+Router.run(routes, function(Handler){
+  React.render(<Handler/>, document.getElementById('app'));
+});
