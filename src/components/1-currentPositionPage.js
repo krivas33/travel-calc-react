af7b94ce8bae087a91eb4ec01a489cdd629e8a2c
Kevin Rivas <krivas2433@gmail.com>
2016-08-11 10:31:19 -0400

    Linked with router from splashPage

--- src/components/1-currentPositionPage.js
+++ src/components/1-currentPositionPage.js
@@ -1,8 +1,27 @@
-
+/* globals require, module */
 "use strict";
-
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
+var RouteHandler = require('react-router').RouteHandler;
 
-var currentPositionPage =
+
+var currentPositionPage = React.createClass({
+  render: function() {
+    return (
+      <div className="jumbotron">
+        <h1>Where are you located?</h1>
+        <p>Input or select your current locoation.</p>
+        <hr/>
+        <p></p>
+        <p class="lead">
+          <a class="btn btn-primary btn-lg" href="#" role="button">Next step!</a>
+        </p>
+      </div>
+    );
+  }
+});
 
 module.exports = currentPositionPage;
+
+//Popover button more more info.
