8add80fcbb2d730c24e6d9aa3e2890f7e3744d79
Kevin Rivas <krivas2433@gmail.com>
2016-08-11 10:33:19 -0400

    Moved and imported Handler and global to app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,22 +1,21 @@
 /*eslint-disable strict */ //Disabled check because we can't run strict mode. Need global vars.
+/* globals require, module */
 
 var React = require('react');
-//var Header = require('./common/header');
-//var RouteHandler = require('react-router').RouteHandler;
-var $ = null;
-var jQuery = null;
+var RouteHandler = require('react-router').RouteHandler;
 $ = jQuery = require('jquery');
 
 var App = React.createClass({
   render: function() {
+
     return (
       <div>
-        <Header/>
-        <div className="container-fluid">
-        <RouteHandler/>
-        </div>
+          <div className="container-fluid">
+            <RouteHandler/>
+          </div>
       </div>
     );
+
   }
 });
 
