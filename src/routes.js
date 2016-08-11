9bebc3d4380944c7e02ab9aed859f83d49d67652
Kevin Rivas <krivas2433@gmail.com>
2016-08-11 10:34:57 -0400

    Added the routes for all pages

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,20 @@
 /* globals require, module */
-//"use strict";
+"use strict";
 
-//var React = require('react');
-//var Router = require('react-router');
-//var DefaultRoute = Router.DefaultRoute;
-//var Route = Router.Route;
-//var NotFoundRoute = Router.NotFoundRoute;
-//var Redirect = Router.Redirect;
+var React = require('react');
+var Router = require('react-router');
+var DefaultRoute = Router.DefaultRoute;
+var Route = Router.Route;
 
-//var routes = (
-//  <Route name="app" path="/" handler={require('./components/app')}>
-//  <DefaultRoute handler={require('./components/0-splashPage')} />
-//  <Route name="currentPosition" handler={require('./components/1-currentPositionPage')} />
-//  <Route name="goingPage" handler={require('./components/2-goingPage')} />
-//  <Route name="vehiclePage" handler={require('./components/3-vehiclePage')} />
-//  <Route name="gasPage" handler={require('./components/4-gasPage')} />
-//  <Route name="summaryPage" handler={require('./components/5-summaryPage')} />
-//  </Route>
-//);
+var routes = (
+ <Route name="app" path="/" handler={require('./components/app')}>
+   <DefaultRoute handler={require('./components/0-splashPage')} />
+   <Route name="currentPositionPage" handler={require('./components/1-currentPositionPage')} />
+   <Route name="goingPage" handler={require('./components/2-goingPage')} />
+   <Route name="vehiclePage" handler={require('./components/3-vehiclePage')} />
+   <Route name="gasPage" handler={require('./components/4-gasPage')} />
+   <Route name="summaryPage" handler={require('./components/5-summaryPage')} />
+ </Route>
+);
 
-//module.exports = routes;
+module.exports = routes;
