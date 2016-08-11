9b473470b7879a4de691f2e6379b882ed48c0334
Kevin Rivas <krivas2433@gmail.com>
2016-08-11 11:18:41 -0400

    Added Route for splashPage

--- src/routes.js
+++ src/routes.js
@@ -14,6 +14,7 @@ var routes = (
    <Route name="vehiclePage" handler={require('./components/3-vehiclePage')} />
    <Route name="gasPage" handler={require('./components/4-gasPage')} />
    <Route name="summaryPage" handler={require('./components/5-summaryPage')} />
+   <Route name="splashPage" handler={require('./components/0-splashPage')} />
  </Route>
 );
 
