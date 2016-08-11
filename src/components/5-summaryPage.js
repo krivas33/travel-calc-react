fce2693364cdf0823dd27ecbe3801482c7e759d6
Kevin Rivas <krivas2433@gmail.com>
2016-08-11 11:18:19 -0400

    Routed from gasPage and added button to Link to beginning; to splashPage

--- src/components/5-summaryPage.js
+++ src/components/5-summaryPage.js
@@ -1,8 +1,26 @@
-
+/* globals require, module */
 "use strict";
-
 var React = require('react');
+var {Link} = require('react-router');
+var RouteHandler = require('react-router').RouteHandler;
 
-var summaryPage =
+
+var summaryPage = React.createClass({
+  render: function() {
+    return (
+      <div className="jumbotron">
+        <h1>You summary!</h1>
+        <p></p>
+        <hr/>
+        <p></p>
+        <p class="lead">
+          <Link to="splashPage" class="btn btn-primary btn-lg" role="button">New calculation</Link>
+        </p>
+      </div>
+    );
+  }
+});
 
 module.exports = summaryPage;
+
+//Popover button more more info.
