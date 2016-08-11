8e6c4dc8da90688115e9aff6d2463e5b76c3b8d4
Kevin Rivas <krivas2433@gmail.com>
2016-08-11 11:17:34 -0400

    Routed from vehiclePage and Linked to summaryPage

--- src/components/4-gasPage.js
+++ src/components/4-gasPage.js
@@ -1,8 +1,26 @@
-
+/* globals require, module */
 "use strict";
-
 var React = require('react');
+var {Link} = require('react-router');
+var RouteHandler = require('react-router').RouteHandler;
 
-var gasPage =
+
+var gasPage = React.createClass({
+  render: function() {
+    return (
+      <div className="jumbotron">
+        <h1>Which gas station do you frequent?</h1>
+        <p>Select desired brand.</p>
+        <hr/>
+        <p></p>
+        <p class="lead">
+          <Link to="summaryPage" class="btn btn-primary btn-lg" role="button">See your results</Link>
+        </p>
+      </div>
+    );
+  }
+});
 
 module.exports = gasPage;
+
+//Popover button more more info.
