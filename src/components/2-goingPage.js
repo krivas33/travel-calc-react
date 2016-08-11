773ea07f670fb524710164fd19f158959fcd2759
Kevin Rivas <krivas2433@gmail.com>
2016-08-11 11:16:38 -0400

    Routed from currentPositionPage and Linked to vehiclePage

--- src/components/2-goingPage.js
+++ src/components/2-goingPage.js
@@ -1,8 +1,26 @@
-
+/* globals require, module */
 "use strict";
-
 var React = require('react');
+var {Link} = require('react-router');
+var RouteHandler = require('react-router').RouteHandler;
 
-var goingPage =
+
+var goingPage = React.createClass({
+  render: function() {
+    return (
+      <div className="jumbotron">
+        <h1>Where are you going?</h1>
+        <p>Input your destination's address.</p>
+        <hr/>
+        <p></p>
+        <p class="lead">
+          <Link to="vehiclePage" class="btn btn-primary btn-lg" role="button">Next step!</Link>
+        </p>
+      </div>
+    );
+  }
+});
 
 module.exports = goingPage;
+
+//Popover button more more info.
