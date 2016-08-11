b32b781fe4701ff78a8605f50062a450649707e3
Kevin Rivas <krivas2433@gmail.com>
2016-08-11 13:24:26 -0400

    Added vehicleInput page

---
+++ src/components/3.1-vehicleInput.js
@@ -0,0 +1,28 @@
+"use strict";
+var React = require('react');
+var {Link} = require('react-router');
+var RouteHandler = require('react-router').RouteHandler;
+
+
+var vehicleInput = React.createClass({
+  render: function() {
+    return (
+      <div className="jumbotron">
+        <h1>Input your car specifications</h1>
+        <p>Then save.</p>
+        <hr/>
+        <p></p>
+          <p class="lead">
+            <Link to="vehiclePage" class="btn btn-primary btn-lg" role="button">Cancel</Link>
+          </p>
+        <p class="lead">
+          <Link to="vehiclePage" class="btn btn-primary btn-lg" role="button">Save</Link>
+        </p>
+      </div>
+    );
+  }
+});
+
+module.exports = vehicleInput;
+
+//Popover button more more info.
