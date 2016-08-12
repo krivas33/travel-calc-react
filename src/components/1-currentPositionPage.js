dd6334fd12cfa948b805fdc52aa21a082dec231a
Kevin Rivas <krivas2433@gmail.com>
2016-08-12 09:37:55 -0400

    Added image and myStyle

--- src/components/1-currentPositionPage.js
+++ src/components/1-currentPositionPage.js
@@ -1,29 +1,35 @@
 /* globals require, module */
 "use strict";
-var React = require('react');
 var {Link} = require('react-router');
+var React = require('react');
 var RouteHandler = require('react-router').RouteHandler;
 
+var headerStyle = {
+  background: "url('images/whereYouLocated.png') center / cover"
+};
 
 var currentPositionPage = React.createClass({
   render: function() {
     return (
-      <div className="jumbotron">
-        <h1>Where are you located?</h1>
-        <p>Input or select your current locoation.</p>
+      <div className="background hero">
+        <div className="row image" style={headerStyle}>
+          <h2 className="text-center">Where are you located?</h2>
+        </div>
+
+        <div className="row">
+          <p id="description" className="text-center">Input or select your current locoation.</p>
+        </div>
+
         <hr/>
-        <p></p>
-          <p class="lead">
-            <Link to="splashPage" class="btn btn-primary btn-lg" role="button">Back</Link>
+
+        <div className="row">
+          <p className="text-center lead">
+            <Link className="nextButton btn btn-primary btn-lg" to="goingPage" role="button">Next step!</Link>
           </p>
-        <p class="lead">
-          <Link to="goingPage" class="btn btn-primary btn-lg" role="button">Next step!</Link>
-        </p>
+        </div>
       </div>
     );
   }
 });
 
 module.exports = currentPositionPage;
-
-//Popover button more more info.
