7ed50ad585a5128d4c652e310020791947e27d15
Kevin Rivas <krivas2433@gmail.com>
2016-08-12 09:37:38 -0400

    Added myStyle and image

--- src/components/0-splashPage.js
+++ src/components/0-splashPage.js
@@ -3,23 +3,40 @@
 var React = require('react');
 var {Link} = require('react-router');
 
+var logoStyle = {
+  width: '100%'
+};
 
 var splashPage = React.createClass({
   render: function() {
     return (
       <div className="jumbotron">
-        <h1>Travel Calc</h1>
-        <p>Calculate the cost of your trip using your car model, trip route and favorite gas station.</p>
+
+        <div className="row">
+          <h1 className="text-center">Travel Calc</h1>
+        </div>
+
+        <div className="row">
+          <img className="col-sm-12" src="images/map.png"/>
+        </div>
+
+        <div className="row">
+          <p className="text-center">Calculate the cost of your trip using your car model, trip route and favorite gas station.</p>
+        </div>
+
         <hr/>
-        <p>Traveler's Calculator</p>
-        <p class="lead">
-          <Link to="currentPositionPage" className="btn btn-primary btn-lg" role="button">Lets get started!</Link>
-        </p>
+
+        <p className="text-center">Traveler's Calculator</p>
+
+        <div className="row">
+          <p className="lead text-center">
+            <Link id="start-button" to="currentPositionPage" className="btn btn-primary btn-lg" role="button" class="col-md-8">Lets get started!</Link>
+          </p>
+        </div>
+
       </div>
     );
   }
 });
 
 module.exports = splashPage;
-
-//Popover button more more info.
