80d57aa2a00952960d041f03344ab8fc66dda76f
Kevin Rivas <krivas2433@gmail.com>
2016-08-12 10:21:39 -0400

    Styled splash page

--- src/components/0-splashPage.js
+++ src/components/0-splashPage.js
@@ -10,27 +10,25 @@ var logoStyle = {
 var splashPage = React.createClass({
   render: function() {
     return (
-      <div className="jumbotron">
+      <div id="splash-page">
 
         <div className="row">
           <h1 className="text-center">Travel Calc</h1>
         </div>
 
         <div className="row">
-          <img className="col-sm-12" src="images/map.png"/>
+          <img className="col-sm-8 offset-sm-2" src="images/map.png"/>
         </div>
 
         <div className="row">
-          <p className="text-center">Calculate the cost of your trip using your car model, trip route and favorite gas station.</p>
+          <p id="tagline" className="text-center col-sm-8 offset-sm-2">Calculate the cost of your trip using your car model, route and favorite gas station.</p>
         </div>
 
         <hr/>
 
-        <p className="text-center">Traveler's Calculator</p>
-
         <div className="row">
           <p className="lead text-center">
-            <Link id="start-button" to="currentPositionPage" className="btn btn-primary btn-lg" role="button" class="col-md-8">Lets get started!</Link>
+            <Link id="start-button" to="currentPositionPage" className="btn btn-primary btn-lg" role="button" class="col-md-8">Get started</Link>
           </p>
         </div>
 
